7e7a8834b21fa7622d4c193f6cc12af4ab1c2665
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-09-09 15:58:17 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 76547
+var accountEmail = "prashan@gmail.com"
+let accountPassword = "2345"
+accountCity = "Bahadurgarh"
+
+accountEmail = "aerofly@gmail.com"
+accountPassword = "1468"
+console.table([accountId,accountEmail,accountPassword]);
