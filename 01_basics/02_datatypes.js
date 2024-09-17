8753cc7d3f3a95a1d34bbc70a279e066932de8f6
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-09-17 15:20:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// Data types in javaScript//
+/*
+number => 2 to power 53
+bignit
+String => true/False
+null => standallone value 
+undefined =>
+symbol => unique
+object
+*/
+console.log(typeof "Prashan");
+console.log(typeof null);
+console.log(typeof undefined);
