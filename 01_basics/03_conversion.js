e54678a81de89ec086d48c65554e93e5cd9927c4
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-09-17 18:05:46 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,74 @@
+/*
+let score = "33"
+console.log(typeof score);
+*/
+
+// here we will get the output that this is String. So to we will now convert it into the number.
+
+/*
+let score = "33" 
+let typeOfNumber = Number(score)
+console.log(typeof typeOfNumber);
+*/
+
+//What is i write "33abc" then it will be nuber or not or what will be the output is we print simple. and what will be the type of it.
+/*
+let score = "33abc"
+let typeOfNumber = Number(score)
+console.log(typeof typeOfNumber)
+console.log(typeOfNumber)
+*/
+/*
+let score = null
+console.log(typeof score);
+console.log(score);
+let valueOfScore = Number(score)
+console.log(typeof valueOfScore);
+console.log(valueOfScore);
+*/
+
+/*
+let score = true
+console.log(typeof score);
+console.log(score);
+let valueOfScore = Number(score)
+console.log(typeof valueOfScore);
+console.log(valueOfScore);
+*/
+
+/*
+let score = false
+console.log(typeof score);
+console.log(score);
+let valueOfScore = Number(score)
+console.log(typeof valueOfScore);
+console.log(valueOfScore);
+*/
+
+/*
+let score = 1
+let valueOfScore = Boolean(score)
+console.log(valueOfScore);
+*/
+
+/*
+let score = 0
+let valueOfScore = Boolean(score)
+console.log(valueOfScore);
+*/
+
+/*
+let score = ""
+let valueOfScore = Boolean(score)
+console.log(valueOfScore);
+*/
+
+let score = "Prashan"
+let valueOfScore = Boolean(score)
+console.log(valueOfScore);
+
+
+
+
+
+ 
