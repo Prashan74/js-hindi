d0ce5b16c656145ba1fd2beabe0990160749df6c
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-09-20 18:05:04 +0000

    dataconversion

---
+++ 01_basics/04_datatypeconv.js
@@ -0,0 +1,27 @@
+/*
+console.log(2 + 2);
+
+console.log("2" + 2)
+
+console.log(2 + "2")
+
+console.log("2" + 2 + 2)
+
+console.log(2 + 2 + "2");
+
+console.log(2 + "2" + 2);
+
+console.log(+true);
+
+console.log(true+); // error
+
+let gameScore = 100
+gameScore++
+console.log(gameScore);
+
+let gameScore = 100
+++gameScore
+console.log(gameScore);
+*/
+
+console.log(+false);
