4b9c1ca95d27d5bafcf2b81647fe2a874d61676e
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-09-22 15:43:10 +0000

    premitive

---
+++ 01_basics/06_pndatatype.js
@@ -0,0 +1,33 @@
+//Premitive data types are :- String, number, Boolean, Symbol, BigInt, null, Undefined.
+
+//Non-Premitive data types are :- Array, Objects, Function.
+
+//Example of Symbol dat tyoe which is also a premitive data type.
+/*
+let id = Symbol('123')
+let newId = Symbol('123')
+console.log(id === newId);
+
+*/
+//Example of Non-Premitive data types:-
+// first array :-
+/*
+const heros = ["sushant", "sahrukh", "vicky"];
+console.log(heros);
+
+*/
+//Example of Object:-
+/*
+let obj = {
+    name : "Prashan",
+    age : 22
+
+}
+console.log(obj);
+*/
+// Example of Function:-
+const newFunction = function(){
+    "hello world"
+    
+}
+console.log(newFunction);
