f76d65e82bcb563b24210822574d11b067476b2c
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-09-23 19:29:20 +0000

    string

--- 01_basics/07_strings.js
+++ 01_basics/07_strings.js
@@ -9,11 +9,29 @@ const myname = "Prashan"
 const youname = "Verma"
 console.log(` ${myname} ${youname} `);
 
-*/
 // There is another way also to declare a variable in strings
 const myname = new String("Prashan")
 console.log(myname[1]);
 console.log(myname.length);
 console.log(myname.charAt(4));
 console.log(myname.indexOf("h"));
+console.log(myname.toUpperCase());
+console.log(myname.toLowerCase());
+console.log(myname.substring(0, 4));
+console.log(myname.substring(-6, 4)); // in substring it will not obey negetive index at first place. it will print same as (0, 4) will print.
+console.log(myname.slice(0, 4));
+console.log(myname.slice(-6, 4)); // in slicing it will obey negetive index at first 
+
+*/
+const newname = "  Prashan  "
+console.log(newname);
+
+console.log(newname.trim()); // it used to remove extra space before or after the string.
+console.log(newname.replace("Prashan", "Prasan"));
+console.log(newname.includes("Prashan")); // it tells wheather the term is exist in the provided variable which is 'newname' it checks that 'Prashan' is there or not in 'newname' and it was there so it will give in output 'True'
+
+
+
+
+
 
