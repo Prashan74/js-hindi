0f4ffac23b2aeec0fc02ddb124c075b12902258f
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-09-23 17:01:21 +0000

    string

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,19 @@
+/*
+const myname = "Prashan"
+const youname = "Verma"
+console.log(myname + " " + youname);
+
+// There is another way also to add them. by using this sign ` ` and to add ${}
+
+const myname = "Prashan"
+const youname = "Verma"
+console.log(` ${myname} ${youname} `);
+
+*/
+// There is another way also to declare a variable in strings
+const myname = new String("Prashan")
+console.log(myname[1]);
+console.log(myname.length);
+console.log(myname.charAt(4));
+console.log(myname.indexOf("h"));
+
