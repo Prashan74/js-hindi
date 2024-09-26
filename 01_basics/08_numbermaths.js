3ccdd83a4fe2c3f2652e7775c7ac09d0f5627e2c
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-09-26 15:19:40 +0000

    numbermaths

---
+++ 01_basics/08_numbermaths.js
@@ -0,0 +1,20 @@
+const score = 100
+console.log(score);
+
+const value = new Number(100)
+console.log(value);
+console.log(value.toString())
+console.log(value.toString().length)
+console.log(value.toFixed(2))
+
+const grade = 25.668
+console.log(grade.toPrecision(3));
+
+const number = 1000000
+console.log(number.toLocaleString());
+//it will give in the american format means like this 1,000,000 but we ant in indian so we have do like this
+console.log(number.toLocaleString('en-IN')); // it will give in indian formate which is 10,00,000
+
+
+
+
