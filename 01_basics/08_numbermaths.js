2991700e85afda707b87efd5b436db88f350a5b3
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2024-10-12 17:41:45 +0000

    number

--- 01_basics/08_numbermaths.js
+++ 01_basics/08_numbermaths.js
@@ -1,3 +1,4 @@
+/*
 const score = 100
 console.log(score);
 
@@ -14,6 +15,12 @@ const number = 1000000
 console.log(number.toLocaleString());
 //it will give in the american format means like this 1,000,000 but we ant in indian so we have do like this
 console.log(number.toLocaleString('en-IN')); // it will give in indian formate which is 10,00,000
+*/
+/////////////////// Maths /////////////////////
+
+console.log(Math.abs(-4));
+const value = 5.7
+console.log(Math.round(value));
 
 
 
