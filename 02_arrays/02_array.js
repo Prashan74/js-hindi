c7ad2ba910c307bab8bc61c919a6b5b96aad66b2
Prashan74 <141302789+Prashan74@users.noreply.github.com>
2025-08-28 18:25:42 +0000

    array

--- 02_arrays/02_array.js
+++ 02_arrays/02_array.js
@@ -1,4 +1,10 @@
-
+/*
 const myArray = [1,2,3,4]
 console.log(myArray);
 console.log(myArray[1]);
+*/
+const mycar = ["BMW","Tata","Sizuki","Mahindra"]
+console.log(mycar);
+console.log(mycar[3]);
+
+
